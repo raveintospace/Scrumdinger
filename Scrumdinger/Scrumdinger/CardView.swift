0ca397cedecf4d946201f9629797edb34faa0fb5
Oriol Angelet <uriaeri@gmail.com>
2025-09-23 10:35:09 +0200

    Added CardView, DailyScrum model & sample // Added ThemeKit package

---
+++ Scrumdinger/Scrumdinger/CardView.swift
@@ -0,0 +1,39 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Uri on 23/9/25.
+//
+
+import SwiftUI
+import ThemeKit
+
+struct CardView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+                .accessibilityAddTraits(.isHeader)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .labelStyle(.trailingIcon)
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundStyle(scrum.theme.accentColor)
+    }
+}
+
+#Preview(traits: .fixedLayout(width: 400, height: 60)) {
+    let scrum = DailyScrum.sampleData[0]
+    CardView(scrum: scrum)
+        .background(scrum.theme.mainColor)
+}
---
+++ Scrumdinger/Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,16 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Uri on 23/9/25.
+//
+
+import Foundation
+import ThemeKit
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
