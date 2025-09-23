7c3f0ea41abf9ea016d4d17b052469dd8d299047
Oriol Angelet <uriaeri@gmail.com>
2025-09-23 09:50:10 +0200

    Project setup and MeetingView added

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Uri on 23/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
